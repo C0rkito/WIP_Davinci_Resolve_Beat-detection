b739d7bcb430abcb472b2dcf240c79d493386066
C0rkito <hugo.izanic@gmail.com>
2024-04-08 13:31:51 +0200

    Window Button/Text field

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
 #!/usr/bin/env python
 
-resolve = app.GetResolve()
+posX,posY = 0,0
+width,height = 450,250
+path = ""
+
+
+def window(posX,posY,width,height):
+    ui = fusion.UIManager
+    dispatcher = bmd.UIDispatcher(ui)
+
+    win = dispatcher.AddWindow({ 'ID': 'myWindow','Geometry': [posX,posY,width,height]},
+                               [ ui.Label({ 'Text': 'Hello World!' }),
+                                 ui.Button({'ID' : 'Browse','Text':'Browse','Geometry': [width/2,height/2,width/5,height/5]}),
+                                 ui.LineEdit({'ID':'le_1','PlaceholderText':'Name of the file','Events': {'TextEdited': True}})])
+
+    
+    def OnClose(ev):
+            dispatcher.ExitLoop()
+            win.Hide()
+            
+    
+    def OnButtonClicked(ev):
+        global path
+        print(path)
+        dispatcher.ExitLoop()
+        win.Hide()
+
+    def OnLineEditTextEdited(ev):
+        global path
+        path = ev["Text"]
+
+
+
+    win.On.myWindow.Close = OnClose
+    win.On['le_1'].TextEdited = OnLineEditTextEdited
+    win.On['Browse'].Clicked = OnButtonClicked
+    win.Show()
+    dispatcher.RunLoop()
+    
+    
+
+
+window(posX,posY,width,height)
+
+
 
 projectManager = resolve.GetProjectManager()
 project = projectManager.GetCurrentProject()
