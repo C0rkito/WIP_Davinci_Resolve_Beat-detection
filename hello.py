a62632ff1de105137c27687a460d8a937a61e2a4
C0rkito <hugo.izanic@gmail.com>
2024-04-11 00:21:51 +0200

    some tests

--- hello.py
+++ hello.py
@@ -1,49 +1,50 @@
-import librosa
 import sys
 import os
 import librosa
 import numpy as np
 
 
-with open("C:\\Users\\HUGO\\AppData\\Roaming\\Blackmagic Design\\DaVinci Resolve\\Support\\Fusion\\Scripts\\Comp\\Beat_Script\\Davinci-Resolve-Beat-detection\\path.txt","r") as file:
-    audio_file = file.read()
 
-# https://musicinformationretrieval.com/onset_detection.html
+directory = os.getcwd()
 
-file_path = audio_file
+with open(os.getcwd()+"\\path.txt","r") as file:
+    file_path = file.read()
+
+
 
 
 def onset():
     x, sr = librosa.load(file_path)
-    onset_frames = librosa.onset.onset_detect(y=x, sr=sr)
+    onset_frames = librosa.onset.onset_detect(y=x, sr=sr,wait=1, pre_avg=1, post_avg=1, pre_max=1, post_max=1)
     onset_times = librosa.frames_to_time(onset_frames)
     file_name_no_extension, _ = os.path.splitext(file_path)
-    print(onset_times[1])
-    
-    with open("C:\\Users\\HUGO\\AppData\\Roaming\\Blackmagic Design\\DaVinci Resolve\\Support\\Fusion\\Scripts\\Comp\\Beat_Script\\Davinci-Resolve-Beat-detection\\beats.txt", 'w') as f:
+    print(onset_times[50])
+    onset_times.sort()
+    with open(os.getcwd()+"\\beats.txt", 'w') as f:
         f.write('\n'.join(['%.4f' % onset_time for onset_time in onset_times]))
     return onset_times
 def peak():
 
     y, sr = librosa.load(file_path)
     onset_env = librosa.onset.onset_strength(y=y, sr=sr,
-                                             hop_length=512,
-                                             aggregate=np.median)
-    peaks = librosa.util.peak_pick(onset_env)
+                                             hop_length=512)
+    peaks = librosa.util.peak_pick(onset_env, pre_max=1, post_max=1, pre_avg=1, post_avg=1, delta=0.8, wait=1)
     peaks_times = librosa.frames_to_time(peaks, sr=sr, hop_length=512)
-    with open("C:\\Users\\HUGO\\AppData\\Roaming\\Blackmagic Design\\DaVinci Resolve\\Support\\Fusion\\Scripts\\Comp\\Beat_Script\\Davinci-Resolve-Beat-detection\\beats.txt", "w") as file:
+    peaks_times.sort()
+    with open(directory+"\\beats.txt", "w") as file:
         for peak in peaks_times:
             file.write(str(peak)+"\n")
-    print(peaks_times[1])
+    print(peaks_times[50])
     return peaks_times
 
 def beat():
     y, sr = librosa.load(file_path)
     tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
     beat_times = librosa.frames_to_time(beat_frames, sr=sr)
-    print(beat_times[1])
+    print(beat_times[50])
     
-    with open("C:\\Users\\HUGO\\AppData\\Roaming\\Blackmagic Design\\DaVinci Resolve\\Support\\Fusion\\Scripts\\Comp\\Beat_Script\\Davinci-Resolve-Beat-detection\\beats.txt", "w") as file:
+    beat_times.sort()
+    with open(directory+"\\beats.txt", "w") as file:
         for beat_time in beat_times:
             file.write(str(beat_time)+"\n")
     return beat_times
@@ -61,7 +62,7 @@ def beat_2():
 
 
     beats_dynamic.sort()
-    with open('C:\\Users\\HUGO\\AppData\\Roaming\\Blackmagic Design\\DaVinci Resolve\\Support\\Fusion\\Scripts\\Comp\\Beat_Script\\Davinci-Resolve-Beat-detection\\beats.txt', 'w') as file:
+    with open(directory+'\\beats.txt', 'w') as file:
         for beat_time in beats_dynamic:
             file.write(str(beat_time) + '\n')
     return beats_dynamic
@@ -70,21 +71,21 @@ def big_beat(beats_arrays):
     occurrences = {}
     for algo in range(len(beats_arrays)):
         for element in beats_arrays[algo]:
-            if element in occurrences:
+            if (element) in occurrences:
                 occurrences[element] += 1
             else:
                 occurrences[element] = 1
-    
-    non_uniques = [element for element, count in occurrences.items() if count > 1]
+
+    non_uniques = [element for element, count in occurrences.items() if count >= 2]
     return non_uniques
     
-#onset_tab = onset()
+onset_tab = onset()
 peak_tab = peak()
-#beat_tab = beat()
+beat_tab = beat()
 #beat2_tab = beat_2()
 
 tab = big_beat([onset_tab,peak_tab,beat_tab])
-with open ('C:\\Users\\HUGO\\AppData\\Roaming\\Blackmagic Design\\DaVinci Resolve\\Support\\Fusion\\Scripts\\Comp\\Beat_Script\\Davinci-Resolve-Beat-detection\\beats.txt', 'w') as file:
+with open (directory+'\\beats.txt', 'w') as file:
     for line in tab:
         file.write(str(line)+"\n")
 
